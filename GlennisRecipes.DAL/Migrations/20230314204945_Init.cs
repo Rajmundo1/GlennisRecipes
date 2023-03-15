4c0f3e1bac240c17bcc22034b75d911c0c600fa5
Rajmundo1 <rajmund.knapp@gmail.com>
2023-03-15 10:11:11 -0700

    Create Db Context and Seed

---
+++ GlennisRecipes.DAL/Migrations/20230314204945_Init.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GlennisRecipes.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AspNetRoles",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUsers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RefreshToken = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoleClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ClaimType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClaimValue = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoleClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetRoleClaims_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ClaimType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClaimValue = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserLogins",
+                columns: table => new
+                {
+                    LoginProvider = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProviderKey = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProviderDisplayName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserLogins", x => new { x.LoginProvider, x.ProviderKey });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserRoles",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RoleId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserTokens",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    LoginProvider = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Instructions = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OwnerId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Recipes_AspNetUsers_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserComments",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RecipeId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserComments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserComments_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_UserComments_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRatings",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RecipeId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRatings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserRatings_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_UserRatings_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "4fe86bb9-bf7e-46ec-a5b9-b29d38929611", "2fd44e77-ece5-4253-8620-3daf7ee6dfd6", "Normal", "Normal" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "RefreshToken", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { "7a0f0fc8-de75-4231-bca4-f02595b8fc22", 0, "b3de81c8-4c77-4f5a-9bc9-529183ef2e34", "jill@mail.com", false, false, null, "jill@mail.com", "Jill", "AQAAAAEAACcQAAAAEOR5ijcLEUUQDcVq5cX6cEOugcVoMtFZpKU7BcPIluN3dG9mazOUAUkYYB301/wrdA==", null, false, null, "22f06de7-a46a-4571-914b-bd218e4daf0b", false, "Jill" },
+                    { "81139b48-6c24-4fc8-ad72-6c1423608d83", 0, "9136f156-a628-4333-a4a7-6ae34d1f08d9", "bakerpeter@mail.com", false, false, null, "bakerpeter@mail.com", "Baker Peter", "AQAAAAEAACcQAAAAEN39HbGb2pg9QmF4kPKWxddr6SltiIdEVlCjEU6sR37/lfThibWsIRAlxSTcL5Xidw==", null, false, null, "39fa66b6-8b18-4486-8c9c-2fd273eddf93", false, "Baker Peter" },
+                    { "a8b93270-4150-4411-a54b-6521de4a49aa", 0, "7799d6ca-db6f-4a2d-86b1-4975830ca48d", "chefjoe@mail.com", false, false, null, "chefjoe@mail.com", "Chef Joe", "AQAAAAEAACcQAAAAEN9kFnNLrqg5xJAUogi8t2/DrUDTSvBGRoK3BYc/ug47X+CRLJZ3a0YqmaxAFd0BNQ==", null, false, null, "0628a652-2a60-4edc-9967-b464336d2c1f", false, "Chef Joe" },
+                    { "b295bea7-efc3-42a7-8e26-ea3fd5c99ec6", 0, "03c23694-5ed3-4eb2-ae34-330278160b9f", "jake@mail.com", false, false, null, "jake@mail.com", "Jake", "AQAAAAEAACcQAAAAEL6vR/x3q5tItCYZqY7uDszxtgsnbsBDLvItgMVrf21qorJjgS1F8iOBy3LZM+PmDQ==", null, false, null, "2e846cb8-40b1-4f89-b004-3b1d205027b1", false, "Jake" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[,]
+                {
+                    { "4fe86bb9-bf7e-46ec-a5b9-b29d38929611", "7a0f0fc8-de75-4231-bca4-f02595b8fc22" },
+                    { "4fe86bb9-bf7e-46ec-a5b9-b29d38929611", "81139b48-6c24-4fc8-ad72-6c1423608d83" },
+                    { "4fe86bb9-bf7e-46ec-a5b9-b29d38929611", "a8b93270-4150-4411-a54b-6521de4a49aa" },
+                    { "4fe86bb9-bf7e-46ec-a5b9-b29d38929611", "b295bea7-efc3-42a7-8e26-ea3fd5c99ec6" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Recipes",
+                columns: new[] { "Id", "ImagePath", "Instructions", "Name", "OwnerId" },
+                values: new object[,]
+                {
+                    { "0fccd974-5b7e-4157-887d-87ce146756e1", "", "This is how you can create a Chocolate Cake", "Chocolate Cake", "a8b93270-4150-4411-a54b-6521de4a49aa" },
+                    { "20bf6a09-4f7f-45c0-80df-defdfb494656", "", "This is how you can create a Red Velvet Cake", "Red Velvet Cake", "a8b93270-4150-4411-a54b-6521de4a49aa" },
+                    { "604f5685-08d4-40a0-94a1-63f309bb07e7", "", "This is how you can create a Lemon Sponge Cake", "Lemon Sponge Cake", "81139b48-6c24-4fc8-ad72-6c1423608d83" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "UserComments",
+                columns: new[] { "Id", "Comment", "RecipeId", "UserId" },
+                values: new object[] { "6d721ff4-8c3e-4d01-9ae9-a80310ecd988", "This cake was amazing !!!!!", "20bf6a09-4f7f-45c0-80df-defdfb494656", "b295bea7-efc3-42a7-8e26-ea3fd5c99ec6" });
+
+            migrationBuilder.InsertData(
+                table: "UserRatings",
+                columns: new[] { "Id", "Rating", "RecipeId", "UserId" },
+                values: new object[,]
+                {
+                    { "611b0a25-1fd0-46d8-a534-10e986150ba1", 5, "0fccd974-5b7e-4157-887d-87ce146756e1", "b295bea7-efc3-42a7-8e26-ea3fd5c99ec6" },
+                    { "66e453c5-d55b-47be-9773-5d9aa8ea8aaa", 3, "20bf6a09-4f7f-45c0-80df-defdfb494656", "7a0f0fc8-de75-4231-bca4-f02595b8fc22" },
+                    { "c23d966b-9e78-4b8a-9e9d-c50be6d0b9a5", 3, "0fccd974-5b7e-4157-887d-87ce146756e1", "7a0f0fc8-de75-4231-bca4-f02595b8fc22" },
+                    { "d95b7961-a1b1-4ab4-9e80-465f6516d453", 5, "604f5685-08d4-40a0-94a1-63f309bb07e7", "b295bea7-efc3-42a7-8e26-ea3fd5c99ec6" },
+                    { "d9a6be63-0c3a-4feb-9cda-eae06d2b3509", 5, "20bf6a09-4f7f-45c0-80df-defdfb494656", "b295bea7-efc3-42a7-8e26-ea3fd5c99ec6" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoleClaims_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserClaims_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserLogins_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_OwnerId",
+                table: "Recipes",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserComments_RecipeId",
+                table: "UserComments",
+                column: "RecipeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserComments_UserId",
+                table: "UserComments",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRatings_RecipeId",
+                table: "UserRatings",
+                column: "RecipeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRatings_UserId",
+                table: "UserRatings",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AspNetRoleClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserLogins");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "UserComments");
+
+            migrationBuilder.DropTable(
+                name: "UserRatings");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles");
+
+            migrationBuilder.DropTable(
+                name: "Recipes");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUsers");
+        }
+    }
+}
---
+++ GlennisRecipes.DAL/Migrations/20230314223654_Add-Image-Paths.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GlennisRecipes.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImagePaths : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4fe86bb9-bf7e-46ec-a5b9-b29d38929611",
+                column: "ConcurrencyStamp",
+                value: "968d9eb4-b2e3-49d7-87bf-ffa3fe3264c9");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "7a0f0fc8-de75-4231-bca4-f02595b8fc22",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "f0c13a7c-7ee3-44be-8480-812407c86ace", "AQAAAAEAACcQAAAAEBXjZGn1q2pxtESReNiNgliUSfKsTFzN0ItgeHbHhWFNdVswgBBe0iXTSnzckfG5hA==", "f7e1adb6-969a-4dc2-8d5c-33c59d53238e" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "81139b48-6c24-4fc8-ad72-6c1423608d83",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "3360c207-67a1-47c9-834f-d116a57a7b44", "AQAAAAEAACcQAAAAEB0mPll/84h3Z1EAZeD14OjzUVtI0Mx+EUx35Yz3UYdTJer2a1z2xK5uoou/QptNPw==", "9d8f4d73-705f-496d-a804-2c0334fd15f9" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a8b93270-4150-4411-a54b-6521de4a49aa",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "f00decd4-c379-45b4-b767-a8684baa22fa", "AQAAAAEAACcQAAAAENQcSjJm5l8f5fyDPqk579BTmHHuOpPTwwmeEjfwRLyns6gshONfnF4R2YjMDCcV/Q==", "ddd6afd7-8eab-4cd2-87c9-2020586a3753" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "b295bea7-efc3-42a7-8e26-ea3fd5c99ec6",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "006d3d4d-425c-4aa7-80fe-044cbc1ccd47", "AQAAAAEAACcQAAAAEMZFovJ8EqI6EOcRZ6JEC/8IZTj0mM36euclwuTw2VNxLZUUgwFpldL2nUuH539dRw==", "12bf7e55-f527-4c05-9c92-7c2e8dbbd98a" });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: "0fccd974-5b7e-4157-887d-87ce146756e1",
+                column: "ImagePath",
+                value: "ChocolateCake202303142235021967.png");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: "20bf6a09-4f7f-45c0-80df-defdfb494656",
+                column: "ImagePath",
+                value: "RedVelvetCake202303142234211747.png");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: "604f5685-08d4-40a0-94a1-63f309bb07e7",
+                column: "ImagePath",
+                value: "LemonSpongeCake202303142234497511.png");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4fe86bb9-bf7e-46ec-a5b9-b29d38929611",
+                column: "ConcurrencyStamp",
+                value: "2fd44e77-ece5-4253-8620-3daf7ee6dfd6");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "7a0f0fc8-de75-4231-bca4-f02595b8fc22",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "b3de81c8-4c77-4f5a-9bc9-529183ef2e34", "AQAAAAEAACcQAAAAEOR5ijcLEUUQDcVq5cX6cEOugcVoMtFZpKU7BcPIluN3dG9mazOUAUkYYB301/wrdA==", "22f06de7-a46a-4571-914b-bd218e4daf0b" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "81139b48-6c24-4fc8-ad72-6c1423608d83",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "9136f156-a628-4333-a4a7-6ae34d1f08d9", "AQAAAAEAACcQAAAAEN39HbGb2pg9QmF4kPKWxddr6SltiIdEVlCjEU6sR37/lfThibWsIRAlxSTcL5Xidw==", "39fa66b6-8b18-4486-8c9c-2fd273eddf93" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a8b93270-4150-4411-a54b-6521de4a49aa",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "7799d6ca-db6f-4a2d-86b1-4975830ca48d", "AQAAAAEAACcQAAAAEN9kFnNLrqg5xJAUogi8t2/DrUDTSvBGRoK3BYc/ug47X+CRLJZ3a0YqmaxAFd0BNQ==", "0628a652-2a60-4edc-9967-b464336d2c1f" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "b295bea7-efc3-42a7-8e26-ea3fd5c99ec6",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "03c23694-5ed3-4eb2-ae34-330278160b9f", "AQAAAAEAACcQAAAAEL6vR/x3q5tItCYZqY7uDszxtgsnbsBDLvItgMVrf21qorJjgS1F8iOBy3LZM+PmDQ==", "2e846cb8-40b1-4f89-b004-3b1d205027b1" });
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: "0fccd974-5b7e-4157-887d-87ce146756e1",
+                column: "ImagePath",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: "20bf6a09-4f7f-45c0-80df-defdfb494656",
+                column: "ImagePath",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: "604f5685-08d4-40a0-94a1-63f309bb07e7",
+                column: "ImagePath",
+                value: "");
+        }
+    }
+}
